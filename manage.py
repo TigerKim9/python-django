b1b66f3e0fb7914c62b86ddf0b4cd35265d43e59
TigerKim9 <kkm9499@gmail.com>
2023-06-20 00:58:38 +0900

    disable tensorRT

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EF.settings')
+    
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
