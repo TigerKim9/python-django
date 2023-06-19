2377174313d67f06195a185af967273a2c605af3
TigerKim9 <kkm9499@gmail.com>
2023-06-20 01:08:55 +0900

    disable gpu in apps

--- AreYouIdol/apps.py
+++ AreYouIdol/apps.py
@@ -5,6 +5,7 @@ import os
 
 class AreyouidolConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     name = 'AreYouIdol'
     model = load_model('model/model_vgg16_ver1.h5')
     img_path = os.path.join('media', 'images')
